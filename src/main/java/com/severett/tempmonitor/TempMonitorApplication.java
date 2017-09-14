1373c7e7c46c5a574d2c596033f37d3fa3a7069c
severn-everett <severn.everett@gmail.com>
2017-09-14 21:33:18 +0200

    Initial commit

---
+++ src/main/java/com/severett/tempmonitor/TempMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.severett.tempmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempMonitorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/severett/tempmonitor/TempMonitorApplicationTest.java
@@ -0,0 +1,16 @@
+package com.severett.tempmonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TempMonitorApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
